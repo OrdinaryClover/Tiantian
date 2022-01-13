ef81cd15d3e493a54150380c19a341e7b9bc267a
woo <1975964827@qq.com>
2022-01-14 00:15:30 +0800

    test

--- Tiantian/Tiantian/views.py
+++ Tiantian/Tiantian/views.py
@@ -4,6 +4,7 @@
 #@File  :views.py.py
 #@Software  :PyCharm
 from django.http import HttpResponse
+from django.template import Template,Context
 def index(request):
     """
     视图
@@ -29,7 +30,32 @@ def retest1(request,year,city):
     result = "我在%s"%city
     return HttpResponse(result01)
 
-def retest2(request,year,city):
-    result01 = "我%s年在%s"%(year,city)
-    result = "我在%s"%city
-    return HttpResponse(result01)
+def indexhtml(request,age):
+    """
+    编写一个html
+    :param request:
+    :return:
+    """
+    html = """
+    <html>
+        <head></head>
+        <body>
+            <h1>我是index页面</h1>
+            <img src = "https://t7.baidu.com/it/u=1732966997,2981886582&fm=193&f=GIF">
+        </body><br>
+        姓名：{{ name }}<br>
+        年龄：{{ age }}
+    </html>
+    """
+
+    #返回一个响应对象
+    # return HttpResponse(html)
+    #渲染动态的数据
+    #1.创建模板
+    template_obj = Template(html)
+    #2.构建动态数据
+    parmas = {"name":"gutianle","age":age}
+    content_obj = Context(parmas)
+
+    result = template_obj.render(content_obj)
+    return HttpResponse(result)
